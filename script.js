14faefc2d30e26cad0e4c4a41ce5c75e6bb5d3a2
tyrannosaurush3x <94508646+tyrannosaurush3x@users.noreply.github.com>
2024-08-30 10:52:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,33 +5,37 @@ myHeaders.append("Content-Type", "application/json");
 //   console.log("Event data ", event?.data);
 //   console.log("Client ID", event.clientId);
 //   var shop_id = event.clientId
-//   var raw = JSON.stringify({
-//     "action": "hello world",
-//     "user": {
-//       "id": shop_id,
-//       attributes: {
-//         "shopifyId": shop_id
-//       }
-//     }
-//   });
-//   var requestOptions = {
-//     method: 'POST',
-//     headers: myHeaders,
-//     body: raw,
-//     redirect: 'follow'
-//   };
-//   fetch("https://sacramentokings.us-6.evergage.com/api2/event/shopify_test", requestOptions)
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
-// });
 
-analytics.subscribe('cart_viewed', (event) =>{
-  var line_items = 	event.data.cart.lines;
+
+analytics.subscribe('cart_viewed', (event) => {
+  var shop_id = event.clientId
+  var line_items = event.data.cart.lines;
   var ids = []
-  for (var i=0; i<line_items.length; i++) {
+  for (var i = 0; i < line_items.length; i++) {
     var id = line_items[i].merchandise.product.id
-    ids.push(id)
+    ids.push({ "_id": id })
   }
-  console.log("ids are here", ids)
+  var raw = JSON.stringify({
+    "user": {
+      "id": shop_id
+    },
+    "itemAction": "View Cart",
+    "cart": {
+      "complete": {
+        "Product": ids
+      }
+    }
+  }
+  );
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+  fetch("https://sacramentokings.us-6.evergage.com/api2/event/shopify_test", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
 })
