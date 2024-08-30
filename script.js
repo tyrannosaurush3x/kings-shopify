3fb9d70096b70ce3b381c466fbeee43d39e774da
tyrannosaurush3x <94508646+tyrannosaurush3x@users.noreply.github.com>
2024-08-30 11:04:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,41 @@ analytics.subscribe('cart_viewed', (event) => {
     .catch(error => console.log('error', error));
 
 })
+
+analytics.subscribe('checkout_completed', (event) => {
+  // Example for accessing event data
+  const checkout = event.data.checkout;
+
+  const checkoutTotalPrice = checkout.totalPrice?.amount;
+
+  const checkoutId = checkout.token;
+  const checkoutId2 = event.id;
+  console.log("ID1", checkoutId)
+  console.log("ID2", checkoutId2)
+  var raw = JSON.stringify({
+    "user": {
+      "id": shop_id
+    },
+    "itemAction": 'Purchase',
+    "order": {
+      "Product": {
+        "orderId": checkoutId,
+        "lineItems": [
+          {"id": "123"}
+        ]
+      }
+    }
+  }
+  );
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+  // fetch("https://sacramentokings.us-6.evergage.com/api2/event/shopify_test", requestOptions)
+  //   .then(response => response.text())
+  //   .then(result => console.log(result))
+  //   .catch(error => console.log('error', error));
+});
+
